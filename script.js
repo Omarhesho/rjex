2f5e9f9cfd4f780caf57f1eff78ac2d55a07b759
Omarhesho <omarhesho22@gmail.com>
2026-08-18 13:55:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const ORDER_EMAIL = "rjex134@gmail.com";
 
 const products = [
   {id:1, category:'drinks', name:'موخيتو ليمون ونعناع', description:'ليمون منعش مع النعناع الطازج ولمسة غازية خفيفة، طعم منعش ومثالي لأي وقت.', price:35, image:'asset/mojito-lemon-mint.jpeg'},
-  {id:2, category:'drinks', name:'موخيتو توت', description:'مزيج منعش من التوت والليمون والنعناع، بطعم فاكهي لذيذ ولمسة منعشة.', price:40, image:'asset/mohito-berry.jpeg'},
-  {id:3, category:'drinks', name:'موخيتو فراولة', description:'فراولة حلوة ومنعشة مع الليمون والنعناع، تركيبة خفيفة بطعم فاكهي مميز.', price:40, image:'asset/mojito-strawberry.jpeg'},
+  {id:2, category:'drinks', name:'موخيتو توت', description:'مزيج منعش من التوت والليمون والنعناع، بطعم فاكهي لذيذ ولمسة منعشة.', price:40, image:'asset/mojito-berry.jpeg'},
+  {id:3, category:'drinks', name:'موخيتو فراولة', description:'فراولة حلوة ومنعشة مع الليمون والنعناع، تركيبة خفيفة بطعم فاكهي مميز.', price:40, image:'asset/mohito-strawberry.jpeg'},
   {id:4, category:'drinks', name:'فخفخيتو', description:'مزيج مثلج من آيس كريم التوت، كولا بطعم التوت، النعناع والثلج، لطعم فاكهي بارد ومنعش.', price:45, image:'asset/fakfakhito.jpeg'},
   {id:5, category:'noodles', name:'اندومي خضار حار', description:'اندومي بنكهة الخضار الحارة، بطعم غني وتتبيلة حارة لعشاق النكهة القوية.', price:20, image:'asset/indomie-vegetable-spicy.jpeg'},
   {id:6, category:'noodles', name:'اندومي خضار', description:'اندومي بنكهة الخضار بطعم متوازن وتتبيلة لذيذة، من غير حرارة.', price:20, image:'asset/indomie-vegetable.jpeg'},
